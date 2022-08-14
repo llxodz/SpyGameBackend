d8f217ae6e6757f6ad650835dbe83cee65331053
Ilya Gavrilov <llxodz@gmail.com>
2022-08-14 21:45:49 +0300

    Added allCategories reques

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const { log }  = require('./log')
+const express = require("express");
 
-log('hello world!')
+const appExpress = express();
+
+const value = {
+  hello: "world!"
+}
+
+appExpress.get("/allCategories", function(req, res) {
+  res.send(value)
+})
+
+appExpress.listen(3000, function(){
+  log("server is running on port 3000");
+})
