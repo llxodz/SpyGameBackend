dab25761c6bdae1ba3f5d2a672b8bc0523f8801d
Ilya Gavrilov <llxodz@gmail.com>
2023-12-17 16:28:39 +0300

    Added a new page with privacy policy

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { log }  = require('./log')
 const express = require("express");
 const { categoriesStub } = require('./categories+stub')
 const router = express.Router();
+const path = require('path')
 
 const appExpress = express();
 
 let value = categoriesStub()
+let publicPath = path.join(__dirname, 'public')
 
-const allCategories = router.get("/", async (req, res) => {
+appExpress.get("/allCategories", async (req, res) => {
   try {
     res.send(value)
   } catch (error) {
@@ -15,7 +17,13 @@ const allCategories = router.get("/", async (req, res) => {
   }
 })
 
-appExpress.use("/allCategories", allCategories)
+appExpress.get("/privacy_policy", async (req, res) => {
+  try {
+    res.sendFile(`${publicPath}/privacy.html`)
+  } catch (error) {
+    return res.status(500).send("Server error")
+  }
+})
 
 const port = process.env.PORT || 9001;
 appExpress.listen(port, () => console.log(`Listening to port ${port}`));
